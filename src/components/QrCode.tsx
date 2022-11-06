0305560f8ec85b5a8b48ee24fb2c9be97c0870bb
Alexander Davis <alexander.davis.098@gmail.com>
2022-11-06 20:42:35 -0300

    (update): Update the component to use dark and light mode

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -19,18 +19,18 @@ export function QrCode() {
 
     function handleQrCode(event: ChangeEvent<HTMLInputElement>) {
         event.preventDefault()
-        
+
         setLink(event.target.value)
         handleGenerate(event.target.value)
     }
 
     return (
-        <div className='w-screen h-screen bg-black flex flex-col px-4 gap-4 items-center justify-center text-black'>
+        <div className='w-screen h-screen flex flex-col px-4 gap-4 items-center justify-center dark:text-white'>
             <QRCode value={link} />
 
-            <div className='flex items-center h-12 w-full max-w-[400px] py-4 px-3 rounded bg-emerald-50 focus-within:ring-2 ring-emerald-500'>
+            <div className='flex items-center h-12 w-full max-w-[400px] py-4 px-3 rounded bg-slate-200 dark:bg-slate-800 focus-within:ring-2 ring-slate-900 dark:ring-sky-500'>
                 <input
-                    className='bg-transparent flex-1 text-black text-xs placeholder:text-black outline-none'
+                    className='bg-transparent flex-1 text-slate-500 dark:text-slate-300 text-xs placeholder:text-slate-500 dark:placeholder:text-slate-300 outline-none'
                     type='text'
                     placeholder='Digite seu link...'
                     value={link}
@@ -39,7 +39,7 @@ export function QrCode() {
             </div>
 
             <a
-                className='flex items-center justify-between px-4 h-14 w-full max-w-[180px] bg-emerald-500 rounded font-semibold text-black text-md transition-colors hover:bg-emerald-600 cursor-pointer focus:ring-2 ring-white'
+                className='flex items-center justify-between px-4 h-14 w-full max-w-[180px] bg-slate-900 dark:bg-sky-500 rounded font-semibold text-white text-md transition-colors hover:bg-slate-700 dark:hover:bg-sky-400 cursor-pointer focus:ring-2 ring-white'
                 href={qrCodeLink} download={`qrcode.png`}
             >
                 Baixar QR Code
