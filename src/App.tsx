0a88b54f7e4e941033fa2e256b54833dffb4cab7
Alexander Davis <alexander.davis.098@gmail.com>
2022-11-06 20:40:38 -0300

    (feat): Add theme context usage for the entire application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Header } from './components/Header'
 import { QrCode } from './components/QrCode'
+import { ThemeContextProvider } from './contexts/ThemeContext'
 
 import './styles/main.css'
 
 function App() {
-  return (
-    <QrCode />
-  )
+    return (
+        <ThemeContextProvider>
+            <Header />
+            <QrCode />
+        </ThemeContextProvider>
+    )
 }
 
 export default App
